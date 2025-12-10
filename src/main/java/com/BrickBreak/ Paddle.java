e73b0402acfe4a1180eeba506fcca1f6ff56816a
melinozturk <MelinOzturk0707@gmail.com>
2025-12-10 16:02:42 +0000

    lalalala

--- src/main/java/com/BrickBreak/ Paddle.java
+++ src/main/java/com/BrickBreak/ Paddle.java
@@ -8,18 +8,61 @@ import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
-
-
 	//your code here!
 
+	//don't forget you need instance variables:
+	private int x;
+	private int y; 
+	private int width;
+	private int height;
+
+	//constructor(s):
+	public Paddle(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
 
 	
-	//don't forget you need instance variables:
-	
-	
-	//constructor(s):
-	
 	//methods:
-	
+
+	//precondition: g is a non-null value
+	//postcondition: draws the paddle on the screen
+	public void draw(Graphics g) {
+      g.setColor(Color.WHITE);
+
+	  g.fillRect(x, y, width, height);
+	}
+	//precondition:
+	//postcondition: 
+	public void move() {
+		
+	}
+	//precondition:
+	//postcondition: 
+	public void getX(){
+
+	}
+	//precondition:
+	//postcondition: 
+	public void getY(){
+
+	}
+	//precondition:
+	//postcondition: 
+	public void getHeight(){
+
+	}
+	//precondition:
+	//postcondition: 
+	public void getWidth(){
+
+	}
+	//precondition:
+	//postcondition: 
+	public void getVelocity(){
+
+	}
 	
 }
--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -13,17 +13,96 @@ public class Ball {
 	private int x;
 	private int y;
     private int radius;
+	private int xVelocity;
+	private int yVelocity; 
 
 	//constructor(s):
+	public Ball(int x, int y, int radius) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.xVelocity = -1;
+		this.yVelocity = -2;
+	}
 	
 	//methods:
+
+//precondition: 
+//postcondition: 
+
 	public void draw(Graphics g) {
       g.setColor(Color.PINK);
-	 
+	
 	  g.fillOval(x, y, radius, radius);
 	}
-	public void move() {
-	  
-	}
 
+//precondition: None
+//postcondition: moves the ball by changeX and changeY
+
+    public void move() {
+    x+= changeX;
+    y+= changeY;
+	}
+//precondition: 
+//postcondition: 
+	public void setXpos(){
+
+	}
+//precondition: 
+//postcondition: 
+	public void getXpos(){
+
+	}
+//precondition: 
+//postcondition: 
+	public void setYPos(){
+
+	}
+//precondition: 
+//postcondition: 
+	public void getYpos(){
+
+	}
+//precondition: 
+//postcondition: 
+	public void reverseX(){
+
+	}
+//precondition: 
+//postcondition: 
+	public void reverseY(){
+
+	}
+//precondition: 
+//postcondition: 
+	public void setXvelocity(){
+
+	}
+//precondition: 
+//postcondition: 
+	public void getXvelocity(){
+
+	}
+//precondition: 
+//postcondition: 
+	public void setYvelocity(){
+
+	}
+//precondition: 
+//postcondition: 
+	public void getYvelocity(){
+
+	}
+//precondition: 
+//postcondition: 
+	public void setSize(){
+		
+	}
+//precondition: 
+//postcondition: 
+	public void getSize(){
+
+	}
+	
+	
 }
